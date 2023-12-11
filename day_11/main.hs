6e1c17432571622564a9a913903c996ba022b88a
Vlad Ene <vlad@recourseai.com>
2023-12-11 12:04:32 +0000

    Day 11 part 2

--- day_11/main.hs
+++ day_11/main.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldr" #-}
 import System.Environment
+import Data.List (sort)
+import Control.Monad (liftM2)
 
 type Galaxy = (Int, Int)
 
@@ -30,10 +32,36 @@ distance (x1 , y1) (x2 , y2) = abs (x1-x2) + abs (y1-y2)
 
 part1 :: [Galaxy] -> Int
 -- The list comp has each pair twice
-part1 galaxies = (`div` 2) $ sum (uncurry distance <$> [(g1, g2) | g1 <- galaxies, g2 <- galaxies, g1 /= g2])
-
+part1 galaxies = (`div` 2) $ sum (uncurry distance <$> gPairs) 
+    where gPairs = [(g1, g2) | g1 <- galaxies, g2 <- galaxies, g1 /= g2]
 
 -- Part 2
+-- In part 1 we took the navie approach of actually expanding the space and then calculating
+-- which is no longer feasible for part to. We can take the expansion into account when calculating distances
+-- i.e. how many rows & cols between the two galaxies have expanded
+type Expansion = Int
+
+getExpandedIdx :: [String] -> [Int]
+getExpandedIdx = map fst . filter (\(_, v) -> isEmpty v) . indexed
+
+-- Trick to allow us combine two predicates
+-- works because a predicate x -> Bool is a Monad where x is the wrapped value
+(.&&.) = liftM2 (&&)
+
+distanceWithExpansions :: Int -> [Expansion] -> [Expansion] -> Galaxy -> Galaxy -> Int
+distanceWithExpansions f expx expy (x1 , y1) (x2 , y2) = x2'-x1' + dx*(f-1) + y2'-y1' + dy*(f-1)
+    where 
+        [x1', x2'] = sort [x1, x2]
+        [y1', y2'] = sort [y1, y2]
+        dx = length $ filter ((>x1') .&&. (<x2')) expx
+        dy = length $ filter ((>y1') .&&. (<y2')) expy
+
+part2 :: Int -> [Expansion] -> [Expansion] -> [Galaxy] -> Int
+-- The list comp has each pair twice
+part2 f expx expy galaxies = (`div` 2) $ sum (distFunc <$> gPairs) 
+    where 
+        gPairs = [(g1, g2) | g1 <- galaxies, g2 <- galaxies, g1 /= g2]
+        distFunc = uncurry (distanceWithExpansions f expx expy)
 
 main :: IO ()
 main = do
@@ -41,7 +69,11 @@ main = do
     content <- readFile (args !! 0)
     let rows = lines content
 
-    let expanded = expand2D rows
-    let galaxies = parseGalaxies.indexed $ expanded
-    print $ part1 galaxies
+    -- let expanded = expand2D rows
+    -- let galaxies = parseGalaxies.indexed $ expanded
+    -- print $ part1 galaxies
+
+    let galaxies = parseGalaxies.indexed $ rows
+    let (rowExpansions, colExpansions) = (getExpandedIdx rows, (getExpandedIdx.transpose) rows)
+    print $ part2 1000000 rowExpansions colExpansions galaxies
 
